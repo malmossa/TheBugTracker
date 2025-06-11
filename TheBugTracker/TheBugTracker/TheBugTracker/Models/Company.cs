81a07b8483f5d240e5559a0392f4be01f00d605d
malmossa <malmossa@gmail.com>
2025-06-11 09:38:44 -0700

    completed

---
+++ TheBugTracker/TheBugTracker/TheBugTracker/Models/Company.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public Guid? ImageId { get; set; }
+
+        public virtual ImageUpload? Image {  get; set; }
+
+        public virtual ICollection<ApplicationUser> Members { get; set; } = [];
+
+        public virtual ICollection<Project> Projects { get; set; } = [];
+
+        //TODO: many invites
+    }
+}
