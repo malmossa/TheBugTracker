4577fcad05c968076277dc0657e7432e08fd9fab
malmossa <malmossa@gmail.com>
2025-07-23 08:28:44 -0700

    completed

---
+++ TheBugTracker/TheBugTracker/TheBugTracker.Client/Models/TicketHistoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Client.Models
+{
+    public class TicketHistoryDTO
+    {
+        private DateTimeOffset _created;
+
+        [Required]
+        public string? Description { get; set; }
+        public DateTimeOffset Created
+        {
+            get => _created;
+            set => _created = value.ToUniversalTime();
+        }
+        
+        [Required]
+        public string? UserId { get; set; }
+        public UserDTO? User { get; set; }
+    }
+}
--- TheBugTracker/TheBugTracker/TheBugTracker/Models/TicketHistory.cs
+++ TheBugTracker/TheBugTracker/TheBugTracker/Models/TicketHistory.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using TheBugTracker.Client.Models;
 
 namespace TheBugTracker.Models
 {
@@ -19,5 +20,21 @@ namespace TheBugTracker.Models
         [Required]
         public string? UserId { get; set; }
         public virtual ApplicationUser? User { get; set; }
+    }
+
+    public static class TicketHistoryExtensions
+    {
+        public static TicketHistoryDTO ToDTO(this TicketHistory history)
+        {
+            TicketHistoryDTO dto = new TicketHistoryDTO()
+            {
+                Description = history.Description,
+                Created = history.Created,
+                UserId = history.UserId,
+                User = history.User?.ToDTO()
+            };
+
+            return dto;
+        }
     }
 }
