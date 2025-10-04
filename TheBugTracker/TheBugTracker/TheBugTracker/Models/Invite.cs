778bfcac6c52654caa1d0638265f18aae7cf2d62
malmossa <malmossa@gmail.com>
2025-10-04 06:57:14 -0700

    completed

---
+++ TheBugTracker/TheBugTracker/TheBugTracker/Models/Invite.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class Invite
+    {
+        private DateTimeOffset _inviteDate;
+        private DateTimeOffset? _joinDate;
+
+        public int Id { get; set; }
+
+        public DateTimeOffset InviteDate 
+        { 
+            get => _inviteDate; 
+            set => _inviteDate = value.ToUniversalTime();
+        }
+
+        public DateTimeOffset? JoinDate 
+        { 
+            get => _joinDate; 
+            set => _joinDate = value?.ToUniversalTime();
+        }
+
+        public Guid CompanyToken {  get; set; }
+
+        [Required, EmailAddress]
+        public string? InviteeeEmail { get; set; }
+
+        [Required]
+        public string? InviteeeFirstName { get; set; }
+
+        [Required]
+        public string? InviteeeLastName { get; set; }
+
+        public string? Message { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public int CompanyId { get; set; }
+        public virtual Company? Company { get; set; }
+
+        public int ProjectId { get; set; }
+        public virtual Project? Project { get; set; }
+
+        [Required]
+        public string? InvitorId { get; set; }
+        public virtual ApplicationUser? Invitor { get; set; }
+
+        public string? InviteeId { get; set; }
+        public virtual ApplicationUser? Invitee { get; set; }
+    }
+}
