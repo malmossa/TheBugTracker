fbe1f168dc958441f0c86e0940aa6b79ea880562
malmossa <malmossa@gmail.com>
2025-10-08 07:52:53 -0700

    completed

---
+++ TheBugTracker/TheBugTracker/TheBugTracker/Models/Notification.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TheBugTracker.Client.Models.Enums;
+
+namespace TheBugTracker.Models
+{
+    public class Notification
+    {
+        private DateTimeOffset _created;
+
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public string? Message { get; set; }
+
+        public DateTimeOffset Created 
+        { 
+            get => _created; 
+            set => _created = value.ToUniversalTime();
+        }
+
+        public NotificationType Type { get; set; }
+
+        public bool HasBeenViewed { get; set; }
+
+        public int? TicketId { get; set; }
+
+        public virtual Ticket? Ticket { get; set; }
+
+        public int? ProjectId { get; set; }
+
+        public virtual Project? Project { get; set; }
+
+        [Required]
+        public string? RecipientId { get; set; }
+        public virtual ApplicationUser? Recipient { get; set; }
+
+        [Required]
+        public string? SenderId { get; set; }
+        public virtual ApplicationUser? Sender { get; set; }
+    }
+}
